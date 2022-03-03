37a4c33745cb6e972a0691d1aa18baac65509233
errea <erreakezy@gmail.com>
2022-03-03 14:10:16 +0100

    Modify loginForm.vue file

--- components/form/LoginForm.vue
+++ components/form/LoginForm.vue
@@ -3,13 +3,13 @@
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+        Enter Username
       </label>
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
+        Enter Password
       </label>
       <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
       <p class="text-red-500 text-xs italic">Please choose a password.</p>
@@ -19,12 +19,12 @@
         Sign In
       </button>
       <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
+        hAVE YOU Forgottten your Password?
       </a>
     </div>
   </form>
   <p class="text-center text-gray-500 text-xs">
-    &copy;2020 Acme Corp. All rights reserved.
+    &copy;2021 Acme Corp. All Rights Reserved by ERI.
   </p>
 </div>
 </template>
